dd3e23dae809f41f128ad94e61721196a732444a
AnakinSkyR4zor <TheLightShadow@protonmail.com>
2022-05-22 20:34:47 +0100

    Up to "Wall Kicks" on the tutorial

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
+    public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds {
+        get {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+        }
+    }
+
+    private void Awake() {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
+         
+        for (int i = 0; i < this.tetrominoes.Length; i++) {
+            this.tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start() {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece() {
+        int random = Random.Range(0, this.tetrominoes.Length);
+        TetrominoData data = this.tetrominoes[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
+    }
+
+
+    public void Set(Piece piece) {
+        for (int i = 0; i < piece.cells.Length; i++) {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
+
+    public void Clear(Piece piece) {
+        for (int i = 0; i < piece.cells.Length; i++) {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position) {
+        RectInt bounds = this.Bounds;
+
+        for (int i = 0; i < piece.cells.Length; i++) {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition)) {
+                return false;
+            }
+
+            if (this.tilemap.HasTile(tilePosition)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+}
