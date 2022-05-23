a159c760224e83c68373cf7cfe5ec5198bca8e49
AnakinSkyR4zor <TheLightShadow@protonmail.com>
2022-05-23 20:48:51 +0100

    Finished game

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -8,11 +8,19 @@ public class Piece : MonoBehaviour
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
 
+    public float stepDelay = 1f;
+    public float lockDelay = 0.5f;
+
+    private float stepTime;
+    private float lockTime;
+
     public void Initialize(Board board, Vector3Int position, TetrominoData data) {
         this.board = board;
         this.position = position;
         this.data = data;
         this.rotationIndex = 0;
+        this.stepTime = Time.time + this.stepDelay;
+        this.lockTime = 0f;
 
         if (this.cells == null) {
             this.cells = new Vector3Int[data.cells.Length];
@@ -25,6 +33,8 @@ public class Piece : MonoBehaviour
 
     private void Update() {
         this.board.Clear(this);
+
+        this.lockTime += Time.deltaTime;
 
         if (Input.GetKeyDown (KeyCode.Q)) {
             Rotate(-1);
@@ -46,14 +56,36 @@ public class Piece : MonoBehaviour
             HardDrop();
         }
 
+        if (Time.time >= this.stepTime) {
+            Step();
+        }
+
         this.board.Set(this);
+    }
+
+    private void Step() {
+        this.stepTime = Time.time + this.stepDelay;
+
+        Move(Vector2Int.down);
+
+        if (this.lockTime >= this.lockDelay) {
+            Lock();
+        }
     }
 
     private void HardDrop() {
         while (Move(Vector2Int.down)) {
             continue;
         }
-    }       
+
+        Lock();
+    }
+
+    private void Lock() {
+        this.board.Set(this);
+        this.board.ClearLines();
+        this.board.SpawnPiece();
+    }   
 
     private bool Move(Vector2Int translation) {
         Vector3Int newPosition = this.position;
@@ -64,14 +96,26 @@ public class Piece : MonoBehaviour
 
         if (valid) {
             this.position = newPosition;
+            this.lockTime = 0f;
         }
         
         return valid;
     }
 
     private void Rotate(int direction) {
+        int originalRotation = this.rotationIndex;
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
 
+        ApplyRotationMatrix(direction);
+        
+        if (!TestWallKicks(this.rotationIndex, direction)) {
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction) {
+        
         for (int i = 0; i < this.cells.Length; i++) {
             Vector3 cell = this.cells[i];
 
@@ -93,6 +137,30 @@ public class Piece : MonoBehaviour
 
             this.cells[i] = new Vector3Int(x, y, 0);
         }
+    }
+
+    private bool TestWallKicks(int rotationIndex, int rotationDirection) {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+
+        for (int i = 0; i < this.data.wallKicks.GetLength(1); i++) {
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+
+            if (Move(translation)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection) {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection < 0) {
+            wallKickIndex--;
+        }
+
+        return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
     }
 
     private int Wrap(int input, int min, int max) {
